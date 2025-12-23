df219df44fd93cb16d86f3dac93dc0e88cc446e7
harvey emmerson <h.leach-emmerson-2021@hull.ac.uk>
2025-12-23 02:47:05 +0000

    Added base code for main 4 pages, along with global and header css

---
+++ JS/Globals.js
@@ -0,0 +1,16 @@
+function Home()
+{
+    window.location.href = "index.html";
+}
+function About()
+{
+    window.location.href = "About.html";
+}
+function Projects()
+{
+    window.location.href = "Projects.html";
+}
+function Contact()
+{
+    window.location.href = "Contact.html";
+}
